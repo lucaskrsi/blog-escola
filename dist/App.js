917fb34ba792f09eef4bbf470ffb85e711d95d80
Lucas Pereira <lucas.silva.code@outlook.com.br>
2024-09-25 22:00:09 -0300

    feat: cors allow all

--- dist/App.js
+++ dist/App.js
@@ -9,12 +9,17 @@ const router_1 = require("./http/router");
 const errorHandler_1 = require("./http/middlewares/errorHandler");
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const swagger_json_1 = __importDefault(require("./swagger.json"));
+const cors_1 = __importDefault(require("cors"));
 class App {
     constructor() {
         this.server = (0, express_1.default)();
+        this.corsOptions = {
+            origin: "*"
+        };
         this.middlewares();
     }
     middlewares() {
+        this.server.use((0, cors_1.default)(this.corsOptions));
         this.server.use(express_1.default.json());
         this.server.use(router_1.router);
         this.server.use("/api/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
--- src/App.ts
+++ src/App.ts
@@ -3,15 +3,21 @@ import { router } from './http/router';
 import { errorHandler } from './http/middlewares/errorHandler';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './swagger.json';
+import cors from 'cors';
 
 export class App {
     public server: express.Application = express();
+
+    corsOptions = {
+        origin: "*"
+      }
 
     constructor() {
         this.middlewares();
     }
 
     private middlewares() {
+        this.server.use(cors(this.corsOptions))
         this.server.use(express.json());
         this.server.use(router);
         this.server.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
