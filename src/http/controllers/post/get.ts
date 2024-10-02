04af3af02a727d49ec2f2e4399bcbda44622fecb
Lucas Pereira <lucas.silva.code@outlook.com.br>
2024-10-01 23:31:13 -0300

    feat: post return author´s name

--- src/http/controllers/post/get.ts
+++ src/http/controllers/post/get.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
 import { ErrorHandler } from "../../../exceptions/ErrorHandler";
 import { makePostRepository } from "../../../repositories/factory/makePostRepository";
+import { makeUserRepository } from "../../../repositories/factory/makeUserRepository";
 
 export async function get(req: Request, res: Response, next: NextFunction) {
     try {
@@ -12,12 +13,14 @@ export async function get(req: Request, res: Response, next: NextFunction) {
         const { id } = createParam.parse(req.params);
         const postRepository = makePostRepository();
         const post = await postRepository.get(id);
+        const userRepository = makeUserRepository();
+        const author = await userRepository.get(post.getAuthor().getUserId());
         res.status(200).json({
             data: {
                 id: post.getId(),
                 title: post.getTitle(),
                 content: post.getContent(),
-                author: post.getAuthor().getId(),
+                author: author.getName(),
                 published: post.isPublished(),
             },
         });
